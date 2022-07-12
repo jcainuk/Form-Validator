54e4717c67e4c9a7bd073dcd621588164f7845da
Jonathan Cain <jcainuk@gmail.com>
2022-07-13 00:57:36 +0100

    refactor form event listener to check for email validity

--- script.js
+++ script.js
@@ -37,6 +37,8 @@ form.addEventListener('submit', (event) => {
 
   if (email.value === '') {
     showError(email, 'Email is required');
+  } else if (!isValidEmail(email.value)) {
+    showError(email, 'Email is not valid');
   } else {
     showSuccess(email);
   }

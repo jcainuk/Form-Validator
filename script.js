6e007488c41069a1e669ebba8673b8ef7a5cdfc1
Jonathan Cain <jcainuk@gmail.com>
2022-07-13 00:16:05 +0100

    saved all variables from the DOM

---
+++ script.js
@@ -0,0 +1,5 @@
+const form = document.getElementById('form');
+const username = document.getElementById('form');
+const email = document.getElementById('email');
+const password = document.getElementById('password');
+const password2 = document.getElementById('password2');

f906e0260613f5fd00729a8e645c182a67128e7e
Jonathan Cain <jcainuk@gmail.com>
2022-07-13 00:19:34 +0100

    add first form event listener to test it is working and removed default submit behaviour

--- script.js
+++ script.js
@@ -1,5 +1,12 @@
+// Form DOM variables
 const form = document.getElementById('form');
 const username = document.getElementById('form');
 const email = document.getElementById('email');
 const password = document.getElementById('password');
 const password2 = document.getElementById('password2');
+
+// Event Listeners
+form.addEventListener('submit', (event) => {
+  event.preventDefault();
+  console.log('submit');
+});

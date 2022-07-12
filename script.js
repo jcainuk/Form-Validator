af203112e30044a454ffc3a9e7c00ec2f407b6c4
Jonathan Cain <jcainuk@gmail.com>
2022-07-13 00:21:41 +0100

    fix typo problem with username varable

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 // Form DOM variables
 const form = document.getElementById('form');
-const username = document.getElementById('form');
+const username = document.getElementById('username');
 const email = document.getElementById('email');
 const password = document.getElementById('password');
 const password2 = document.getElementById('password2');
@@ -8,5 +8,5 @@ const password2 = document.getElementById('password2');
 // Event Listeners
 form.addEventListener('submit', (event) => {
   event.preventDefault();
-  console.log('submit');
+  console.log(username.value);
 });

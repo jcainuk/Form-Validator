024c2e9015e5791f60b4fddfe103f0798af78849
Jonathan Cain <jcainuk@gmail.com>
2022-07-13 00:52:31 +0100

    write isValidEmail function

--- script.js
+++ script.js
@@ -19,6 +19,12 @@ const showSuccess = (input) => {
   formControl.className = 'form-control success';
 };
 
+// Check email is valid
+const isValidEmail = (email) => {
+  const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  return re.test(String(email).toLowerCase());
+};
+
 // Event Listeners
 form.addEventListener('submit', (event) => {
   event.preventDefault();
